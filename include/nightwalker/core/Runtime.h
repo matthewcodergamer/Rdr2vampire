9cc2f0072ca7df2548e3f7f858fa0a9657212302
matthewcodergamer <gamerstriper@gmail.com>
2026-09-11 14:57:52 -0400

    Phase 3: integrate Shadowstep controller into Runtime

--- include/nightwalker/core/Runtime.h
+++ include/nightwalker/core/Runtime.h
@@ -11,6 +11,7 @@
 #include "nightwalker/game/GameApi.h"
 #include "nightwalker/game/GameContext.h"
 #include "nightwalker/systems/DebugVampireSpawner.h"
+#include "nightwalker/systems/ShadowstepController.h"
 #include "nightwalker/util/Logger.h"
 namespace nightwalker::core {
 class Runtime final {
@@ -19,6 +20,6 @@ public:
  bool Initialize(HMODULE moduleHandle);void Tick();void Shutdown()noexcept;bool IsInitialized()const noexcept{return initialized_;}
 private:
  void ReloadConfig();void CancelSystems()noexcept;void RestoreOwnedState(std::string_view reason)noexcept;
- game::GameContext gameContext_{};util::Logger logger_{};Config config_{};SafetyWatchdog watchdog_{};DebugInput debugInput_{};game::GameApi gameApi_{};systems::DebugVampireSpawner debugVampireSpawner_;std::vector<ILifecycleSystem*> systems_{};util::Deadline debugDebounce_{};std::uint64_t tickCount_{0};std::uint64_t lastTickMs_{0};bool unsafeState_{false};bool initialized_{false};
+ game::GameContext gameContext_{};util::Logger logger_{};Config config_{};SafetyWatchdog watchdog_{};DebugInput debugInput_{};game::GameApi gameApi_{};systems::DebugVampireSpawner debugVampireSpawner_;systems::ShadowstepController shadowstepController_;std::vector<ILifecycleSystem*> systems_{};util::Deadline debugDebounce_{};std::uint64_t tickCount_{0};std::uint64_t lastTickMs_{0};bool unsafeState_{false};bool initialized_{false};
 };
 }
