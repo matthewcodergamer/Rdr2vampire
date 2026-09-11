d62c870f19e5216c9f26e143eb91621367d3be25
matthewcodergamer <gamerstriper@gmail.com>
2026-09-11 14:20:58 -0400

    Phase 1: add debug log level

--- include/nightwalker/util/Logger.h
+++ include/nightwalker/util/Logger.h
@@ -1,27 +1,18 @@
 #pragma once
-
 #include <filesystem>
 #include <fstream>
 #include <mutex>
 #include <string_view>
-
 namespace nightwalker::util {
-
-enum class LogLevel { Info, Warning, Error };
-
+enum class LogLevel { Debug=0, Info=1, Warning=2, Error=3 };
 class Logger final {
 public:
-    Logger() = default;
-    Logger(const Logger&) = delete;
-    Logger& operator=(const Logger&) = delete;
-
-    bool Initialize(const std::filesystem::path& logPath);
-    void Shutdown();
-    void Write(LogLevel level, std::string_view message);
-
+ Logger()=default; Logger(const Logger&)=delete; Logger& operator=(const Logger&)=delete;
+ bool Initialize(const std::filesystem::path& logPath);
+ void Shutdown() noexcept;
+ void SetMinimumLevel(LogLevel level) noexcept { minimumLevel_=level; }
+ void Write(LogLevel level,std::string_view message) noexcept;
 private:
-    std::mutex mutex_{};
-    std::ofstream stream_{};
+ std::mutex mutex_{}; std::ofstream stream_{}; LogLevel minimumLevel_{LogLevel::Info};
 };
-
 }
