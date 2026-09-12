fb9007a6a61a938397c00124e6c9d65852b0cffc
matthewcodergamer <gamerstriper@gmail.com>
2026-09-12 00:36:09 -0400

    Phase 8 verify ragdoll eligibility

--- include/nightwalker/game/GamePhysicalApi.h
+++ include/nightwalker/game/GamePhysicalApi.h
@@ -9,6 +9,7 @@ public:
     virtual ~IGamePhysicalApi() = default;
     [[nodiscard]] virtual bool WasContactFrom(EntityHandle target, EntityHandle source) const noexcept = 0;
     virtual void ClearContactSource(EntityHandle entity) noexcept = 0;
+    [[nodiscard]] virtual bool CanRagdoll(PedHandle ped) const noexcept = 0;
     virtual bool SetRagdoll(PedHandle ped, int durationMs) noexcept = 0;
     virtual bool ApplyImpulse(EntityHandle entity, const Vec3& impulse) noexcept = 0;
 };
@@ -17,6 +18,7 @@ class GamePhysicalApi final : public IGamePhysicalApi {
 public:
     [[nodiscard]] bool WasContactFrom(EntityHandle target, EntityHandle source) const noexcept override;
     void ClearContactSource(EntityHandle entity) noexcept override;
+    [[nodiscard]] bool CanRagdoll(PedHandle ped) const noexcept override;
     bool SetRagdoll(PedHandle ped, int durationMs) noexcept override;
     bool ApplyImpulse(EntityHandle entity, const Vec3& impulse) noexcept override;
 };
