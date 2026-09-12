8dd953d45fa5f5bd453d515b5124a26069e12f95
matthewcodergamer <gamerstriper@gmail.com>
2026-09-11 21:28:14 -0400

    Phase 5 add targeted player Shadowstep planning

--- include/nightwalker/systems/ShadowstepController.h
+++ include/nightwalker/systems/ShadowstepController.h
@@ -8,9 +8,11 @@
 #include "nightwalker/core/ILifecycleSystem.h"
 #include "nightwalker/core/SafetyWatchdog.h"
 #include "nightwalker/game/GameApi.h"
+#include "nightwalker/game/GameCombatApi.h"
 #include "nightwalker/game/GamePresentationApi.h"
 #include "nightwalker/systems/ShadowstepPresentationSettings.h"
 #include "nightwalker/systems/ShadowstepResolver.h"
+#include "nightwalker/systems/TargetedShadowstepPlanner.h"
 #include "nightwalker/util/Logger.h"
 
 namespace nightwalker::systems {
@@ -32,7 +34,11 @@ enum class ShadowstepState {
 
 class ShadowstepController final : public core::ILifecycleSystem {
 public:
-    ShadowstepController(game::IGameApi& api, util::Logger& logger, const core::Config& config) noexcept;
+    ShadowstepController(
+        game::IGameApi& api,
+        game::IGameCombatApi& combatApi,
+        util::Logger& logger,
+        const core::Config& config) noexcept;
 
     [[nodiscard]] std::string_view Name() const noexcept override { return "ShadowstepController"; }
     bool Initialize() override;
@@ -58,9 +64,11 @@ private:
     [[nodiscard]] static const char* StateName(ShadowstepState state) noexcept;
 
     game::IGameApi& api_;
+    game::IGameCombatApi& combatApi_;
     util::Logger& logger_;
     const core::Config& config_;
     ShadowstepResolver resolver_;
+    TargetedShadowstepPlanner targetedPlanner_;
     core::ShadowstepSettings carryResolverSettings_{};
     ShadowstepResolver carryResolver_;
     game::GamePresentationApi presentationApi_{};
@@ -69,16 +77,19 @@ private:
 
     ShadowstepState state_{ShadowstepState::Idle};
     game::PedHandle player_{0};
+    game::PedHandle targetPed_{0};
     game::Vec3 startPosition_{};
     game::Vec3 forwardDirection_{};
     game::Vec3 carryStart_{};
     ShadowstepResolveResult resolution_{};
     ShadowstepResolveResult carryResolution_{};
+    TargetedShadowstepPlan targetedPlan_{};
     std::uint64_t stateStartedMs_{0};
     std::uint64_t cooldownUntilMs_{0};
     std::uint64_t hiddenUntilMs_{0};
     std::uint64_t meleeBufferUntilMs_{0};
     std::uint32_t stressSuccessCount_{0};
+    bool targetedMode_{false};
     bool meleeBuffered_{false};
     bool fxUnavailableLogged_{false};
 };
